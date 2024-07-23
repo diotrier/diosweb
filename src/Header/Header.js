6a94c48d68efca5e0b1c0ffea2b00b679afbba60
diotrier <ramadhantrie9@gmail.com>
2024-07-23 15:47:01 +0700

    first commit

---
+++ src/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Div1 } from './HeaderStyles'
+import Link from 'next/link'
+
+const Header = () => {
+    return (
+        <Container>
+            <Div1>
+                <h1>
+                    this is a header
+                </h1>
+            </Div1>
+        </Container>
+    )
+}
+
+export default Header
