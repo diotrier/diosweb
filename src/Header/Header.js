7fd320372d708f74332410e055a0d2ea13be48fb
diotrier <ramadhantrie9@gmail.com>
2024-07-23 17:15:48 +0700

    fifth

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Container, Div1 } from './HeaderStyles'
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles'
 import { DiCssdeck } from 'react-icons/di';
-import Link from 'next/link'
+import Link from 'next/link';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 
 const Header = () => {
     return (
@@ -13,6 +14,34 @@ const Header = () => {
                     </a>
                 </Link>
             </Div1>
+            <Div2>
+                <li>
+                    <Link href="#projects" legacyBehavior>
+                        <NavLink>Projects</NavLink>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="#tech" legacyBehavior>
+                        <NavLink>Technologies</NavLink>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="#about" legacyBehavior>
+                        <NavLink>About</NavLink>
+                    </Link>
+                </li>
+            </Div2>
+            <Div3>
+                <SocialIcons href="https://google.com">
+                    <AiFillGithub size="3rem" />
+                </SocialIcons>
+                <SocialIcons href="https://google.com">
+                    <AiFillLinkedin size="3rem" />
+                </SocialIcons>
+                <SocialIcons href="https://google.com">
+                    <AiFillInstagram size="3rem" />
+                </SocialIcons>
+            </Div3>
         </Container>
     )
 }
