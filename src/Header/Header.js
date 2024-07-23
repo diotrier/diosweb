409283c6beff08007c2fe6ceb4983cde2cdb7405
diotrier <ramadhantrie9@gmail.com>
2024-07-23 17:04:51 +0700

    fourth

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Container, Div1 } from './HeaderStyles'
+import { DiCssdeck } from 'react-icons/di';
 import Link from 'next/link'
 
 const Header = () => {
     return (
         <Container>
             <Div1>
-                <h1>
-                    Dio trie ramadhann
-                </h1>
+                <Link href="/" legacyBehavior>
+                    <a style={{ display: 'flex', alignItems: 'center', color: "white" }}>
+                        <DiCssdeck size="3rem" /> <span>Portfolio</span>
+                    </a>
+                </Link>
             </Div1>
         </Container>
     )
