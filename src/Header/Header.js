9f24fc45c0a53d23dbec04a63d4e5ba49f1069b9
diotrier <ramadhantrie9@gmail.com>
2024-07-23 16:31:46 +0700

    second commit

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <Container>
             <Div1>
                 <h1>
-                    this is a header
+                    Dio trie ramadhan
                 </h1>
             </Div1>
         </Container>
